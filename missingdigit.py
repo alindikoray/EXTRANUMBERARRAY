dc891b959da84e5496c5da6f6390e1f22db9525b
chris <alindikris77@gmail.com>
2016-10-12 15:03:56 +0300

    one array bares an extra number

---
+++ missingdigit.py
@@ -0,0 +1,14 @@
+"""Given two arrays, having positive integers. One of the arrays will have one extra number,as shown:
+[1,2,3] and [1,2,3,4] should return 4
+[4,66,7] and [66,77,7,4] should return 77 """
+
+def find_missing(first, second):
+    len_first = len(first)
+    len_second = len(second)
+
+    if len_first == len_second:
+        return 0
+    else:
+        for i in second:
+            if i not in first:
+                return i
